46f08c8f4337ca3d68a9f36b2b9b325b0d3bdbe3
Honza Javorek <mail@honzajavorek.cz>
2016-02-20 16:08:09 +0100

    Author field for new articles.

--- scripts/new.py
+++ scripts/new.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import platform
 from datetime import datetime, timedelta
 
 from slugify import slugify
@@ -17,14 +18,25 @@ title = input('Article title? ').strip()
 if not title:
     sys.exit('Error: No title provided.')
 
+# Read author
+if platform.system() == 'Darwin':
+    author_suggestion = 'Honza'
+elif platform.system() == 'Windows':
+    author_suggestion = 'Zuzka'
+else:
+    author_suggestion = 'Zuzka a Honza'
+
+author_prompt = 'Article author? [{}] '.format(author_suggestion)
+author = input(author_prompt).strip() or author_suggestion
+
 # Prepare file name
 slug = slugify(title)
 pub_date = datetime.now() + timedelta(hours=2)
 file_name = '{:%Y-%m-%d}_{}.md'.format(pub_date, slug)
 
 # Prepare file content
-template = 'Title: {}\nDate: {:%Y-%m-%d %H:%M:%S}\n\n'
-file_content = template.format(title, pub_date)
+template = 'Title: {}\nDate: {:%Y-%m-%d %H:%M:%S}\nAuthor: {}\n\n'
+file_content = template.format(title, pub_date, author)
 
 # Write file content
 path = os.path.join(POSTS_DIR, file_name)
