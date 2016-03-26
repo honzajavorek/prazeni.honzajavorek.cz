78c58e6d564da14c87cd6ef2f402cb35abe5645b
Honza Javorek <mail@honzajavorek.cz>
2016-03-26 21:40:25 +0100

    Theming.

--- settings.py
+++ settings.py
@@ -4,10 +4,22 @@ import platform
 from danube_delta.settings import *  # NOQA
 
 
+# Authors
 AUTHOR = 'Zuzka & Honza'
+ABOUT_HEADING = AUTHOR
+
 SITENAME = 'Pražení'
 SITESUBTITLE = 'Nežít Brno'
 
+ABOUT = '''
+V roce 2014 jsme se odstěhovali z Brna do Prahy. Trauma z toho máme tak silné,
+že jsme si v rámci terapie založili tenhle blog o tom, jak se s tím
+vyrovnáváme. Naši kámoši v Brně ho čtou a ťukaj si na čelo.
+'''
+ABOUT_IMAGE = 'images/about.jpg'
+
+
+# Production settings
 if PRODUCTION:
     SITEURL = 'http://prazeni.honzajavorek.cz'
 
@@ -24,3 +36,4 @@ else:
 # Theming
 DISQUS_SITENAME = 'prazeni'
 GOOGLE_ANALYTICS = 'UA-1316071-18'
+GOOGLE_FONTS = ['PT Sans', 'Wellfleet']
